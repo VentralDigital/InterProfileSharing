d56e0a054616cba5895a5829298100dbb759c49e
patrickd <patrick.d@ventral.org>
2026-02-12 17:02:57 +0100

    v1.1 Release prep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "digital.ventral.ips"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
